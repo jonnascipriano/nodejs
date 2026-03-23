75ead2380b759a808506c89b05d6709d33ed20a8
Jonnas Cipriano <jonnascipriano@gmail.com>
2026-03-23 06:32:20 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import http from "http"
 
 const server = http.createServer((req, res) => {
-    const {method} = req
-    return res.writeHead(200).end("Método: " + method)
+    const {method, url} = req
+
+    if(method === "GET" && url === "/products"){
+        return res.end("Lista de produtos!")
+    }
+
+    
+    if(method === "POST" && url === "/products"){
+        return res.writeHead(201).end("Produto cadastrado com sucesso!")
+    }
+
+    return res.writeHead(404).end("Rota não encontrada!")
 })
 
 server.listen(3333)
